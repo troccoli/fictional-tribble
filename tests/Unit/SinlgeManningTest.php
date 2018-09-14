a7f33ce676bfd47f662ae336f7e2279a29d41336
Giulio Troccoli-Allard <giulio@troccoli.it>
2018-09-14 11:55:38 +0100

    Some cosmetic changes to the tests

--- tests/Unit/SinlgeManningTest.php
+++ tests/Unit/SinlgeManningTest.php
@@ -14,6 +14,16 @@ class SinlgeManningTest extends TestCase
 {
     public function testScenarionOne()
     {
+        /*
+         * Scenario 1:
+         *
+         * Given Black Widow working at FunHouse on Monday in one long shift
+         *
+         * When no-one else works during the day
+         *
+         * Then Black Widow receives single manning supplement for the whole duration of her shift.
+         */
+
         // Create a shop and a rota
         $shop = factory(Shop::class)->create([
             'name' => 'Funhouse',
@@ -26,8 +36,8 @@ class SinlgeManningTest extends TestCase
             'week_commence_date' => Carbon::now()->subMonth()->startOfWeek()->format('Y-m-d'),
         ]);
 
-        // Given Black Widow working at FunHouse on Monday in one long shift
-        $staff = factory(Staff::class)->create([
+        /** @var Staff $blackWidow */
+        $blackWidow = factory(Staff::class)->create([
             'first_name' => 'Black',
             'surname'    => 'Widow',
             'shop_id'    => $shop->id,
@@ -38,16 +48,15 @@ class SinlgeManningTest extends TestCase
 
         $shift = factory(Shift::class)->create([
             'rota_id'    => $rota->id,
-            'staff_id'   => $staff->id,
+            'staff_id'   => $blackWidow->id,
             'start_time' => $monday->hour(9)->minute(0)->second(0)->toDateTimeString(),
             'end_time'   => $monday->hour(17)->minute(0)->second(0)->toDateTimeString(),
         ]);
 
-        // When no-one else works during the day
-
-        // Then Black Widow receives single manning supplement for the whole duration of her shift.
+        // Calculate the single mannings
         $singleManningDTO = SingleManningCalculator::calculate($rota);
 
+        // Check the single manning is what we are expecting
         $this->assertEquals($singleManningDTO->monday, 480); // 8 hours shift
         $this->assertEquals($singleManningDTO->tuesday, 0);
         $this->assertEquals($singleManningDTO->wednesday, 0);
@@ -59,6 +68,17 @@ class SinlgeManningTest extends TestCase
 
     public function testScenarioTwo()
     {
+        /*
+         * Scenario 2:
+         *
+         * Given Black Widow and Thor working at FunHouse on Tuesday
+         *
+         * When they only meet at the door to say hi and bye
+         *
+         * Then Black Widow receives single manning supplement for the whole duration of her shift
+         * And Thor also receives single manning supplement for the whole duration of his shift.
+         */
+
         // Create a shop and a rota
         $shop = factory(Shop::class)->create([
             'name' => 'Funhouse',
@@ -71,12 +91,13 @@ class SinlgeManningTest extends TestCase
             'week_commence_date' => Carbon::now()->subMonth()->startOfWeek()->format('Y-m-d'),
         ]);
 
-        // Given Black Widow and Thor working at FunHouse on Tuesday
+        /** @var Staff $blackWidow */
         $blackWidow = factory(Staff::class)->create([
             'first_name' => 'Black',
             'surname'    => 'Widow',
             'shop_id'    => $shop->id,
         ]);
+        /** @var Staff $thor */
         $thor = factory(Staff::class)->create([
             'first_name' => 'Thor',
             'surname'    => '',
@@ -87,27 +108,23 @@ class SinlgeManningTest extends TestCase
         $tuesday = $rota->week_commence_date;
         $tuesday->addDay();
 
-        $shift = factory(Shift::class)->create([
+        $blackWidowShift = factory(Shift::class)->create([
             'rota_id'    => $rota->id,
             'staff_id'   => $blackWidow->id,
             'start_time' => $tuesday->hour(9)->minute(0)->second(0)->toDateTimeString(),
             'end_time'   => $tuesday->hour(12)->minute(0)->second(0)->toDateTimeString(),
         ]);
-        $shift = factory(Shift::class)->create([
+        $thorShift = factory(Shift::class)->create([
             'rota_id'    => $rota->id,
             'staff_id'   => $thor->id,
             'start_time' => $tuesday->hour(12)->minute(0)->second(0)->toDateTimeString(),
             'end_time'   => $tuesday->hour(17)->minute(0)->second(0)->toDateTimeString(),
         ]);
 
-        // When they only meet at the door to say hi and bye
-
-        // Then Black Widow receives single manning supplement for the whole duration of her shift
-
-        // And Thor also receives single manning supplement for the whole duration of his shift.
-
+        // Calculate the single mannings
         $singleManningDTO = SingleManningCalculator::calculate($rota);
 
+        // Check the single manning is what we are expecting
         $this->assertEquals($singleManningDTO->monday, 0);
         $this->assertEquals($singleManningDTO->tuesday, 480); // 4 hours for Black Widow and 4 hours for Thor
         $this->assertEquals($singleManningDTO->wednesday, 0);
