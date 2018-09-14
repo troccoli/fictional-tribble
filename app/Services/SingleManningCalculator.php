e16ef30c1a67f07d8e4a47a67318e86bd71d7cb1
Giulio Troccoli-Allard <giulio@troccoli.it>
2018-09-14 11:22:16 +0100

    Second test passed

--- app/Services/SingleManningCalculator.php
+++ app/Services/SingleManningCalculator.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\DTOs\SingleManningDTO;
 use App\Rota;
+use Illuminate\Support\Carbon;
 use Illuminate\Database\Eloquent\Collection;
 
 class SingleManningCalculator
@@ -16,15 +17,37 @@ class SingleManningCalculator
         $shifts = $rota->shifts;
 
         if ($shifts->isNotEmpty()) {
-            if ($shifts->count() === 1) {
-                $shift = $shifts->pop();
+            $shiftsByDay = $shifts->groupBy(function ($item, $key) {
+                return $item->start_time->format('Y-m-d');
+            });
 
-                // Calculate how many minutes
-                $minutes = $shift->start_time->diffInMinutes($shift->end_time);
+            foreach ($shiftsByDay as $date => $shifts) {
+                $shifts = $shifts->sortBy(function($item, $key) {
+                    return $item->start_time;
+                });
 
-                // Set the DTO
-                $singleManningDTO->addMinutes($shift->start_time, $minutes);
+                if ($shifts->count() === 1) {
+                    $shift = $shifts->pop();
+
+                    // Calculate how many minutes
+                    $minutes = $shift->start_time->diffInMinutes($shift->end_time);
+
+                    // Set the DTO
+                    $singleManningDTO->addMinutes(new Carbon($date), $minutes);
+                } elseif ($shifts->count() === 2) {
+                    $firstShift = $shifts->shift();
+                    $secondShift = $shifts->shift();
+
+                    if ($firstShift->end_time <= $secondShift->start_time) {
+                        $firstShiftMinutes = $firstShift->start_time->diffInMinutes($firstShift->end_time);
+                        $secondShiftMinutes = $secondShift->start_time->diffInMinutes($secondShift->end_time);
+
+                        $singleManningDTO->addMinutes(new Carbon($date), $firstShiftMinutes)
+                            ->addMinutes(new Carbon($date), $secondShiftMinutes);
+                    }
+                }
             }
+
         }
 
         return $singleManningDTO;
--- tests/Unit/SinlgeManningTest.php
+++ tests/Unit/SinlgeManningTest.php
@@ -7,21 +7,16 @@ use App\Services\SingleManningCalculator;
 use App\Shift;
 use App\Shop;
 use App\Staff;
-use Carbon\Carbon;
+use Illuminate\Support\Carbon;
 use Tests\TestCase;
 
 class SinlgeManningTest extends TestCase
 {
     public function testScenarionOne()
     {
-        // Given Black Widow working at FunHouse on Monday in one long shift
+        // Create a shop and a rota
         $shop = factory(Shop::class)->create([
             'name' => 'Funhouse',
-        ]);
-        $staff = factory(Staff::class)->create([
-            'first_name' => 'Black',
-            'surname'    => 'Widow',
-            'shop_id'    => $shop->id,
         ]);
         $rota = factory(Rota::class)->create([
             'shop_id'            => $shop->id,
@@ -30,11 +25,22 @@ class SinlgeManningTest extends TestCase
             // Then we format it as expected
             'week_commence_date' => Carbon::now()->subMonth()->startOfWeek()->format('Y-m-d'),
         ]);
+
+        // Given Black Widow working at FunHouse on Monday in one long shift
+        $staff = factory(Staff::class)->create([
+            'first_name' => 'Black',
+            'surname'    => 'Widow',
+            'shop_id'    => $shop->id,
+        ]);
+
+        /** @var Carbon $monday */
+        $monday = $rota->week_commence_date;
+
         $shift = factory(Shift::class)->create([
             'rota_id'    => $rota->id,
             'staff_id'   => $staff->id,
-            'start_time' => $rota->week_commence_date->hour(9)->minute(0)->second(0),
-            'end_time'   => $rota->week_commence_date->hour(17)->minute(0)->second(0),
+            'start_time' => $monday->hour(9)->minute(0)->second(0)->toDateTimeString(),
+            'end_time'   => $monday->hour(17)->minute(0)->second(0)->toDateTimeString(),
         ]);
 
         // When no-one else works during the day
@@ -44,6 +50,66 @@ class SinlgeManningTest extends TestCase
 
         $this->assertEquals($singleManningDTO->monday, 480); // 8 hours shift
         $this->assertEquals($singleManningDTO->tuesday, 0);
+        $this->assertEquals($singleManningDTO->wednesday, 0);
+        $this->assertEquals($singleManningDTO->thursday, 0);
+        $this->assertEquals($singleManningDTO->friday, 0);
+        $this->assertEquals($singleManningDTO->saturday, 0);
+        $this->assertEquals($singleManningDTO->sunday, 0);
+    }
+
+    public function testScenarioTwo()
+    {
+        // Create a shop and a rota
+        $shop = factory(Shop::class)->create([
+            'name' => 'Funhouse',
+        ]);
+        $rota = factory(Rota::class)->create([
+            'shop_id'            => $shop->id,
+            // We get today's date and get back a month so we are sure to have a whole week in the rota.
+            // The we get the start of the week, which is a Monday because we have set the en_GB locale.
+            // Then we format it as expected
+            'week_commence_date' => Carbon::now()->subMonth()->startOfWeek()->format('Y-m-d'),
+        ]);
+
+        // Given Black Widow and Thor working at FunHouse on Tuesday
+        $blackWidow = factory(Staff::class)->create([
+            'first_name' => 'Black',
+            'surname'    => 'Widow',
+            'shop_id'    => $shop->id,
+        ]);
+        $thor = factory(Staff::class)->create([
+            'first_name' => 'Thor',
+            'surname'    => '',
+            'shop_id'    => $shop->id,
+        ]);
+
+        /** @var Carbon $tuesday */
+        $tuesday = $rota->week_commence_date;
+        $tuesday->addDay();
+
+        $shift = factory(Shift::class)->create([
+            'rota_id'    => $rota->id,
+            'staff_id'   => $blackWidow->id,
+            'start_time' => $tuesday->hour(9)->minute(0)->second(0)->toDateTimeString(),
+            'end_time'   => $tuesday->hour(12)->minute(0)->second(0)->toDateTimeString(),
+        ]);
+        $shift = factory(Shift::class)->create([
+            'rota_id'    => $rota->id,
+            'staff_id'   => $thor->id,
+            'start_time' => $tuesday->hour(12)->minute(0)->second(0)->toDateTimeString(),
+            'end_time'   => $tuesday->hour(17)->minute(0)->second(0)->toDateTimeString(),
+        ]);
+
+        // When they only meet at the door to say hi and bye
+
+        // Then Black Widow receives single manning supplement for the whole duration of her shift
+
+        // And Thor also receives single manning supplement for the whole duration of his shift.
+
+        $singleManningDTO = SingleManningCalculator::calculate($rota);
+
+        $this->assertEquals($singleManningDTO->monday, 0);
+        $this->assertEquals($singleManningDTO->tuesday, 480); // 4 hours for Black Widow and 4 hours for Thor
         $this->assertEquals($singleManningDTO->wednesday, 0);
         $this->assertEquals($singleManningDTO->thursday, 0);
         $this->assertEquals($singleManningDTO->friday, 0);
